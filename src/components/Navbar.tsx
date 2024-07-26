5eb94f183b41d6fc839bd77bda4bb099bf86004f
Avnish-oP <kavnish1245@gmail.com>
2024-07-26 12:36:15 +0530

    about and contact us page done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { motion } from "framer-motion";
 import logo from "../../public/logo.svg";
 
 const Navbar = () => {
@@ -37,22 +38,26 @@ const Navbar = () => {
   return (
     <nav
       className={`fixed w-full text-[#C8A26B] z-20 transition-all duration-300 gilda-display-regular ${
-        scroll ? "bg-white shadow-md" : "bg-transparent"
+        scroll ? "bg-gradient-to-r from-white via-gray-100 to-white shadow-md" : "bg-transparent"
       } ${showNav ? "top-0" : "-top-16"}`}
     >
-      <div className="mx-auto px-4 sm:px-6 lg:px-20">
+      <div className="mx-auto px-2 sm:px-6 lg:px-20">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center space-x-0">
             <Link href="/" className="flex items-center">
-              <Image
-                className={`${
-                  scroll ? "scale-125" : "scale-[1.35]"
-                } brightness-125`}
-                src={logo}
-                alt="Logo"
-                width={100}
-                height={100}
-              />
+              <motion.div
+                initial={{ scale: 1 }}
+                animate={{ scale: scroll ? 1.25 : 1.35 }}
+                transition={{ duration: 0.3 }}
+              >
+                <Image
+                  src={logo}
+                  alt="Logo"
+                  width={100}
+                  height={100}
+                  className="brightness-125"
+                />
+              </motion.div>
               <span className="text-4xl antialiased bellefair-regular font-extralight">
                 INDIANA
               </span>
@@ -63,19 +68,24 @@ const Navbar = () => {
               <Link
                 key={item}
                 href={`/${item.toLowerCase()}`}
-                className="text-[#C8A26B] hover:text-white px-3 py-0 rounded-md text-lg font-medium transition-colors duration-300 cursor-pointer antialiased bellefair-regular"
+                className="relative group text-[#C8A26B] hover:text-[#ffce8a] px-3 py-0 rounded-md text-lg font-medium transition-colors duration-300 cursor-pointer antialiased bellefair-regular"
               >
                 {item}
+                <span
+                  className="absolute left-0 bottom-[-4px] w-full h-[2px] bg-[#C8A26B] scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"
+                ></span>
               </Link>
             ))}
           </div>
           <div className="md:hidden">
-            <button
+            <motion.button
               onClick={toggleMenu}
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-[#C8A26B] hover:text-white hover:bg-[#C8A26B] focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-offset-[#fcead2] focus:ring-white"
+              className="inline-flex items-center justify-center p-2 rounded-md text-[#C8A26B] hover:text-white hover:bg-[#C8A26B] focus:outline-none    transition-transform"
               aria-controls="mobile-menu"
               aria-expanded={isOpen ? "true" : "false"}
+              animate={{ rotate: isOpen ? 45 : 0 }}
+              transition={{ duration: 0.05 }}
             >
               <span className="sr-only">Open main menu</span>
               {isOpen ? (
@@ -111,24 +121,33 @@ const Navbar = () => {
                   />
                 </svg>
               )}
-            </button>
+            </motion.button>
           </div>
         </div>
       </div>
 
-      <div className={`${isOpen ? "block" : "hidden"} md:hidden`} id="mobile-menu">
-        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-          {["Home", "About", "Services", "Contact"].map((item) => (
+      <motion.div
+        className={`${isOpen ? "block" : "hidden"} md:hidden`}
+        id="mobile-menu"
+        initial={{ opacity: 0, height: 0 }}
+        animate={{ opacity: isOpen ? 1 : 0, height: isOpen ? "auto" : 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className="px-2 pt-2 pb-3 space-y-1  sm:px-3">
+          {["Home", "About", "Contacts", "Categories"].map((item) => (
             <Link
               key={item}
               href={`/${item.toLowerCase()}`}
-              className="block text-[#C8A26B] hover:bg-[#ffce8a] hover:text-white px-3 py-2 rounded-md text-base font-medium transition-colors duration-300"
+              className="relative block text-[#C8A26B]  hover:text-[#ffce8a] px-3 py-2 rounded-md text-base font-medium transition-colors duration-300"
             >
               {item}
+              <span
+                  className="absolute left-0 bottom-[-4px] w-full h-[2px] bg-[#C8A26B] scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"
+                ></span>
             </Link>
           ))}
         </div>
-      </div>
+      </motion.div>
     </nav>
   );
 };
