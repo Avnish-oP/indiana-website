522d39eeffe5157fed4b0857a128ace7e8d4331b
Avnish-oP <kavnish1245@gmail.com>
2024-08-02 13:34:43 +0530

    font size changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -113,7 +113,7 @@ const Hero = () => {
               exit="exit"
               className="flex flex-col items-center"
             >
-              {index===0&&<h1 className={`text-sm tracking-wider md:text-lg mb-4 inter-font lg:text-sm font-normal`}>
+              {index===0&&<h1 className={`text-sm tracking-wider md:text-sm mb-4 inter-font lg:text-sm font-normal`}>
                 Welcome to Indiana Merchandising
               </h1>}
               <h1 className={`text-2xl tracking-wider md:text-4xl lg:text-5xl font-normal`}>
